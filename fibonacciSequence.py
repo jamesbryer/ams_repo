5220dad8c9059c0e3f56234d2d6c4f6dfdd9cc53
James E. Bryer <jamesbryer098@gmail.com>
2023-07-11 15:18:01 +0100

    add new file

---
+++ fibonacciSequence.py
@@ -0,0 +1,8 @@
+# def function to generate fibonacci sequence with a given length
+def fibonacciSequence(length):
+    # initialize the sequence with the first two numbers
+    sequence = [0, 1]
+    # loop until the sequence is the desired length
+    while len(sequence) < length:
+        # append the sum of the last two numbers to the sequence
+        sequence.append(sequence[-1] + sequence[-2])
