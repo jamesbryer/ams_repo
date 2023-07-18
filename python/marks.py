c89c4819c70644b24108c92faf9dd94a390c58ef
James E. Bryer <jamesbryer098@gmail.com>
2023-07-18 14:11:22 +0100

    added marks

---
+++ python/marks.py
@@ -0,0 +1,24 @@
+mark = input("Enter your mark: ")
+
+if mark.isdigit():
+    mark = int(mark)
+    if mark >= 85:
+        print("Distinction")
+    elif mark >= 65:
+        print("Pass")
+    else:
+        print("Fail")
+else:
+    print("Not a valid mark")
+
+# now the same code but without using elif
+mark = input("Enter your mark: ")
+
+if mark.isdigit():
+    mark = int(mark)
+    if mark >= 85:
+        print("Distinction")
+    if mark >= 65 and mark < 85:
+        print("Pass")
+    if mark < 65:
+        print("Fail")
