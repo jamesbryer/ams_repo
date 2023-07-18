e268d2ccef468acad523300590f4106b438f0925
James E. Bryer <jamesbryer098@gmail.com>
2023-07-18 14:17:25 +0100

    updates

--- python/marks.py
+++ python/marks.py
@@ -2,12 +2,12 @@ mark = input("Enter your mark: ")
 
 if mark.isdigit():
     mark = int(mark)
-    if mark >= 85:
+    if mark > 100:
+        print("Wow you're literally goated")
+    elif mark >= 85:
         print("Distinction")
     elif mark >= 65:
         print("Pass")
-    elif mark > 100:
-        print("Wow you're literally a goated")
     else:
         print("Fail")
 else:
@@ -19,8 +19,8 @@ mark = input("Enter your mark: ")
 if mark.isdigit():
     mark = int(mark)
     if mark > 100:
-        print("Wow you're literally a goated")
-    if mark >= 85:
+        print("Wow you're literally goated")
+    if mark >= 85 and mark <= 100:
         print("Distinction")
     if mark >= 65 and mark < 85:
         print("Pass")
