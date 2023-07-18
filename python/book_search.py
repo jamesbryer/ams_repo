5f0ac0f641eb472247464b6c8e3724ceb19c71b2
James E. Bryer <jamesbryer098@gmail.com>
2023-07-18 13:45:19 +0100

    sorted added

--- python/book_search.py
+++ python/book_search.py
@@ -9,7 +9,8 @@ books = {
     "Jules Verne": ["Journey to the Center of the Earth", "Twenty Thousand Leagues Under the Sea"],
     "Charlotte Bronte": ["Jane Eyre"],
     "Emily Bronte": ["Wuthering Heights"],
-    "Oscar Wilde": ["The Picture of Dorian Gray", "The Importance of Being Earnest"]
+    "Oscar Wilde": ["The Picture of Dorian Gray", "The Importance of Being Earnest"],
+    "J. K. Rowling": ["Harry Potter and the Philosopher's Stone", "Harry Potter and the Chamber of Secrets", "Harry Potter and the Prisoner of Azkaban", "Harry Potter and the Goblet of Fire", "Harry Potter and the Order of the Phoenix", "Harry Potter and the Half-Blood Prince", "Harry Potter and the Deathly Hallows"]
 }
 
 # ask the user for an author's name
@@ -20,4 +21,4 @@ if authorName not in books.keys():
 else:
     # without looping through the dictionary, we can use the .get() method to get the value of the key
     print("The following books are written by", authorName + ":")
-    print(', '.join(books[authorName]))
+    print(', '.join(sorted(books[authorName])))
