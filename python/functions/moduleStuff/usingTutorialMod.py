0460b08567e7067b6705e43e09451e5ed9250a0e
James E. Bryer <jamesbryer098@gmail.com>
2023-07-19 12:21:11 +0100

    updates

--- python/functions/moduleStuff/usingTutorialMod.py
+++ python/functions/moduleStuff/usingTutorialMod.py
@@ -2,14 +2,10 @@ from tutorialmod import diceRoll
 
 numberOfRolls = int(input("Please enter the number of rolls you would like: "))
 
-if isinstance(numberOfRolls, int):
-    total = 0
-    for i in range(numberOfRolls):
-        roll = diceRoll()
-        print("Roll " + str(i) + ": " + str(roll))
-        total += roll
-    print("The total after " + str(numberOfRolls) + " rolls is: " + str(total) +
-          "\nWhich means an average roll of: " + str(round(total/numberOfRolls, 2)))
-else:
-    numberOfRolls = int(
-        input("Please enter the number of rolls you would like: "))
+total = 0
+for i in range(numberOfRolls):
+    roll = diceRoll()
+    print("Roll " + str(i) + ": " + str(roll))
+    total += roll
+print("The total after " + str(numberOfRolls) + " rolls is: " + str(total) +
+      "\nWhich means an average roll of: " + str(round(total/numberOfRolls, 2)))
