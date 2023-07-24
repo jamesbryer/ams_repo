4828797d518f135ff1662e27a6d34a4104d6f2ba
James E. Bryer <jamesbryer098@gmail.com>
2023-07-24 14:44:52 +0100

    update file_reader

--- python/gui/file_reader.py
+++ python/gui/file_reader.py
@@ -3,29 +3,27 @@ import tkinter
 # the window should have a text box for the file name, a button to read the file, and a text box to display the file contents
 # the file name should be passed to the read_file function
 
+import traceback
+
 
 def read_file():
-    # read the file and display the contents in the label
-    global label
-    # destroy the label if it exists
-    label.destroy()
-    # get the file name from the input box
+    global labels_frame
+    labels_frame.destroy()
+    labels_frame = tkinter.Frame(root)
+    labels_frame.grid(row=2, column=1)
+
     file_name = input1.get()
-    # try to open the file and display the contents in the label
-    try:
-        with open(file_name, 'r') as file:
-            label = tkinter.Label(root, text=file.read())
-            label.grid(row=2, column=1)
-    # if the file is not found, display an error message
-    except FileNotFoundError:
-        label = tkinter.Label(root, text="File not found")
-        label.grid(row=2, column=1)
+    with open(file_name, 'r') as file:
+        for idx, line in enumerate(file):
+            label = tkinter.Label(
+                labels_frame, text=str(idx + 1) + ": " + line.strip())
+            label.grid(row=idx, column=0)
 
 
 # create the gui
 root = tkinter.Tk(className="File Reader")
 # create the label, input box, and button
-label = tkinter.Label(root, text="")
+labels_frame = tkinter.Label(root, text="")
 input1 = tkinter.Entry(root)
 submit = tkinter.Button(root, text="Read File", command=read_file)
 
