8a3a6cfba60b1f61edf0c0df676aa989d3b4f5be
James E. Bryer <jamesbryer098@gmail.com>
2023-07-24 15:10:19 +0100

    added rectangles

---
+++ python/magic_methods/rectangles.py
@@ -0,0 +1,37 @@
+class Rectangle:
+
+    def __init__(self, length: int, width: int) -> None:
+        '''
+        Initializes a rectangle object with a length and width
+        '''
+
+        self.length = length
+        self.width = width
+
+    def __str__(self) -> str:
+        '''
+        Returns an ASCII art representation of the rectangle
+        '''
+
+        # create ASCII art representation of the rectangle
+        rectangle = ""
+        for i in range(self.width):
+            for j in range(self.length):
+                rectangle += "*"
+            rectangle += "\n"
+
+        return rectangle
+
+    def __int__(self) -> int:
+        '''
+        Returns the area of the rectangle
+        '''
+
+        return self.length * self.width
+
+
+if __name__ == "__main__":
+    rectangle = Rectangle(5, 10)
+    print(rectangle)
+    print("The area of the rectangle of size:", rectangle.length,
+          "x", rectangle.width, "is", int(rectangle))
