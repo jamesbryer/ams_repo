c2b6f793b06d9ae872ccb60fb277ae6c42300e05
James E. Bryer <jamesbryer098@gmail.com>
2023-08-07 14:57:18 +0100

    added db stuff

--- flask/intro/app.py
+++ flask/intro/app.py
@@ -1,6 +1,13 @@
 from flask import Flask, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/world"
+#database_uri = 'mysql+pymysql://root:root@localhost:8888/routes'
+
+db = SQLAlchemy(app)
 
 # can use a double decorator to add multiple routes to a function
 @app.route("/")
@@ -14,20 +21,20 @@ def posto():
     response = request.method
     return f"Method is {response}"
 
-@app.route("/name/<word>", methods=['GET', 'POST'])
+@app.route("/name/<word>")
 def name(word):
     var1 = ("<h1 style='color: green'>" + word + "</h1>" + "<br/>") * 5
     return var1
 
 # takes arguements for word and number of times to repeat
-@app.route("/num/<word>/<int:num>", methods=['GET', 'POST'])
+@app.route("/num/<word>/<int:num>")
 def num(word, num):
     var1 = ("<h1 style='color: green'>" + word + "</h1>" + "<br/>") * num
     return var1
 
-@app.route("/square/<int:num>", methods=['GET', 'POST'])
+@app.route("/square/<int:num>")
 def square(num):
-    var1 = num * num
+    var1 = num ** 2
     return str(var1)
 
 # redirects to google
