dc61e01b7c6acf19e0b77fb4a1ccd5deede50d61
James E. Bryer <jamesbryer098@gmail.com>
2023-08-08 13:48:33 +0100

    added better delete functionality

--- flask/crud_tutorial/application/routes.py
+++ flask/crud_tutorial/application/routes.py
@@ -6,14 +6,14 @@ def add(name):
     new_game = Games(name=name)
     db.session.add(new_game)
     db.session.commit()
-    return "Added new game to database"
+    return (f"Added {name} to database")
 
 @app.route('/read')
 def read():
     all_games = Games.query.all()
     games_string = ""
     for game in all_games:
-        games_string += "<br>"+ game.name
+        games_string += "<br>" + str(game.id) + " " + game.name
     return games_string
 
 @app.route('/update/<name>')
@@ -23,9 +23,9 @@ def update(name):
     db.session.commit()
     return first_game.name
 
-@app.route('/delete')
-def delete():
-    game_to_delete = Games.query.first()
+@app.route('/delete/int:<id>')
+def delete(id):
+    game_to_delete = Games.query.filter_by(id=id).first()
     db.session.delete(game_to_delete)
     db.session.commit()
     return "Deleted first game from database"
