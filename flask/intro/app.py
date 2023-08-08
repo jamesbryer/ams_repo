cf7e88c8d57dca0e02b824b82931aae0b7893205
James E. Bryer <jamesbryer098@gmail.com>
2023-08-08 13:37:50 +0100

    tuesday morn

--- flask/intro/app.py
+++ flask/intro/app.py
@@ -4,10 +4,51 @@ import os
 
 app = Flask(__name__)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/world"
-#database_uri = 'mysql+pymysql://root:root@localhost:8888/routes'
+#app.config['SQLALCHEMY_DATABASE_URI'] = "mysql+pymysql://root:@localhost:3306/flask_test"
+app.config['SQLALCHEMY_DATABASE_URI'] = "sqlite:///data.sqlite  "
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URI_INTRO')
+
 
 db = SQLAlchemy(app)
+
+# create a class for the table
+class Person(db.Model):
+    # create columns for the table
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    name = db.Column(db.String(50), nullable=False, default="John Doe")
+    username = db.Column(db.String(50), nullable=False, unique=True)
+    email_address = db.Column(db.String(50), nullable=False, unique=True)
+    age = db.Column(db.Integer)
+
+class Owner(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    first_name = db.Column(db.String(50), nullable=False, default="John")
+    last_name = db.Column(db.String(50), nullable=False, default="Doe")
+    cars = db.relationship('Car', backref='ownerbr')
+
+class Car(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    num_plate = db.Column(db.String(50), nullable=False, default="ABC123")
+    make = db.Column(db.String(50), nullable=False, default="Toyota")
+    model = db.Column(db.String(50), nullable=False, default="Corolla")
+    owner_id = db.Column(db.Integer, db.ForeignKey('owner.id'), nullable=False)
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    order_number = db.Column(db.String(50), nullable=False, default="ABC123")
+    products = db.relationship('Product', backref='orderbr')
+
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    product_name = db.Column(db.String(50), nullable=False, default="ABC123")
+    product_price = db.Column(db.Float, nullable=False, default=0.00)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+
+class Order_Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True, autoincrement=True)
+    order_id = db.Column(db.Integer, db.ForeignKey('order.id'), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey('product.id'), nullable=False)
+    quantity = db.Column(db.Integer, nullable=False, default=1)
 
 # can use a double decorator to add multiple routes to a function
 @app.route("/")
--- flask/intro/create.py
+++ flask/intro/create.py
@@ -1,4 +1,46 @@
-from app import db, app
+from app import db, app, Order, Product, Order_Product, Owner, Car, Person
 
 with app.app_context():
+    db.drop_all()
     db.create_all()
+
+    user = Person(name="John Doe", username="jdoe", email_address="john.doe@gmail.com", age=25)
+    owner = Owner(first_name="John", last_name="Doe")
+    test_car = Car(num_plate="LB15 NKP", make="Volkswagen", model="Polo", ownerbr=owner)
+    test_car_2 = Car(num_plate="BRY33R", make="BMW", model="530d", ownerbr=owner)
+    db.session.add(user)
+    db.session.add(owner)
+    db.session.add(test_car)
+    db.session.commit()
+
+    order = Order(order_number="ABC123")
+
+    db.session.add(order)
+    db.session.commit()
+
+    product = Product(product_name="Hat", product_price=12.99, orderbr=order)
+
+    db.session.add(product)
+    db.session.commit()
+
+    order_product = Order_Product(order_id=order.id, product_id=product.id, quantity=2)
+
+    db.session.add(order_product)
+    db.session.commit()
+
+    test_item = Car.query.filter_by(num_plate="LB15 NKP").first()
+    test_item_2 = Car.query.filter_by(num_plate="BRY33R").first()
+    print(test_item.ownerbr.first_name)
+    print(test_item.num_plate)
+
+    car_to_change = Car.query.filter_by(num_plate="LB15 NKP").first()
+    car_to_change.num_plate = "LB16 NKP"
+    db.session.commit()
+
+    car_to_delete = Car.query.filter_by(num_plate="LB16 NKP").first()
+    db.session.delete(car_to_delete)
+    db.session.commit()
+    
+
+    # Users.query.filter_by(eye_colour="blue").limit(10).order_by(Users.age.desc()).all()
+    
